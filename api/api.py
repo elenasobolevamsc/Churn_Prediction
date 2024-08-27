e415e1e3b3e3a9f483addb9d5b445e814359b452
elena_soboleva <elenasoboleva2003@yandex.ru>
2024-08-27 21:09:30 +0300

    Initial commit

--- api/api.py
+++ api/api.py
@@ -11,6 +11,7 @@ sklearn.set_config(transform_output='pandas')
 
 app = FastAPI()
 
+model_loaded = joblib.load('./best_model.pkl')
 
 class LogData(BaseEstimator, TransformerMixin):
 
@@ -31,9 +32,12 @@ class Dataframe(BaseModel):
     data: str
 
 
-@app.post("/mod")
+@app.post("/best_model")
 async def best_model(one_var: Dataframe):
-    model_loaded = joblib.load('/best_model.pkl')
+
     df = pd.read_json(one_var.data, orient='split')
     pred = model_loaded.predict(df)
+    pred = pd.DataFrame(pred)
     return {"pred": pred.to_json(orient='split')}
+
+
